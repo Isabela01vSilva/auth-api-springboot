7b9676ad10946006beaeddb5315eb5d04a96e9d0
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-06-10 16:02:52 -0300

    init project

---
+++ src/test/java/br/com/Isabela01vSilva/auth_api_security/AuthApiSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Isabela01vSilva.auth_api_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
