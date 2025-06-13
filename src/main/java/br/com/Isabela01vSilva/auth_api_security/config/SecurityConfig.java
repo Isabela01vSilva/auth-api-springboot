4e5cc2cb4f4a41aa1b973bedf92c4a84d1374487
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-06-13 18:53:52 -0300

    feat: Função criar novos usuarios

--- src/main/java/br/com/Isabela01vSilva/auth_api_security/config/SecurityConfig.java
+++ src/main/java/br/com/Isabela01vSilva/auth_api_security/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/login/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
---
+++ src/main/java/br/com/Isabela01vSilva/auth_api_security/controller/dto/CreateUserDTO.java
@@ -0,0 +1,4 @@
+package br.com.Isabela01vSilva.auth_api_security.controller.dto;
+
+public record CreateUserDTO(String username, String password) {
+}
